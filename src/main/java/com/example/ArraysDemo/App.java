5cb5349336b3bd903325ade8811d9073857ceac7
coqui <coqui@HP>
2024-05-29 11:07:14 +0200

    declaracion de un arrays enteros

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -8,6 +8,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /* COMO SE DECLARA UN ARRAYS- 
+          int [] numeros; con los corchetes avisamos de que es un cojunto de valores enteros, 
+          NO SOLO UNO, sino mas de uno. UN ARRAYS
+          
+         */
+    	
+    	int [] numeros;
+    	
+    	/* para acceder a los valores comienzan por 0.*/
+    	/* se añaden valores, primeramente reservando memoria para los elementos en RAM*/
+    	
+    	/* CON EL CONSTRUCTOR NEW*/
+    	    	
+    	numeros = new int [10];
+    	numeros [0] = 1; /* el elemento 0 es el valor, el 1 es el indice que le doy*/
+    	numeros [1] = 2;
+    	
+    	/* y asi hasta el indice 9*/
+    	
     }
 }
