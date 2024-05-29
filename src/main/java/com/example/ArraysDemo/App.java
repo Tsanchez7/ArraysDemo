fd5aec37baa8854db7525dc5db750f483bb2afca
coqui <coqui@HP>
2024-05-29 11:16:12 +0200

    variante dos de declaracion y asignacion de valores en arrays

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -14,18 +14,28 @@ public class App
           
          */
     	
-    	int [] numeros;
+    
     	
     	/* para acceder a los valores comienzan por 0.*/
     	/* se añaden valores, primeramente reservando memoria para los elementos en RAM*/
     	
     	/* CON EL CONSTRUCTOR NEW*/
-    	    	
-    	numeros = new int [10];
-    	numeros [0] = 1; /* el elemento 0 es el valor, el 1 es el indice que le doy*/
-    	numeros [1] = 2;
+    	    // int [] numeros;	
+    //	numeros = new int [10];
+    	//numeros [0] = 1; /* el elemento 0 es el valor, el 1 es el indice que le doy*/
+    //	numeros [1] = 2;
     	
     	/* y asi hasta el indice 9*/
+    	
+    	
+    	
+    	//-------- segunda manera de declarar arrays-------
+    	
+    	
+    		int [] numeros = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    		
+    		string [] nombres = {Tamara, Adrian, Carlos, Luis, Ivan, Maria Jose, Rosa};
+    	
     	
     }
 }
