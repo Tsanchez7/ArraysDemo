47f9e36020780774a5d9220d368b7426e0b1f2f1
coqui <coqui@HP>
2024-05-30 09:46:41 +0200

    Inicio de manejo de arrays objetos

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -6,66 +6,8 @@ package com.example.ArraysDemo;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
-        /* COMO SE DECLARA UN ARRAYS- 
-          int [] numeros; con los corchetes avisamos de que es un cojunto de valores enteros, 
-          NO SOLO UNO, sino mas de uno. UN ARRAYS
-          
-         */
-    
-    
-    	
-    	/* para acceder a los valores comienzan por 0.*/
-    	/* se añaden valores, primeramente reservando memoria para los elementos en RAM*/
-    	
-    	/* CON EL CONSTRUCTOR NEW*/
-    	    // int [] numeros;	
-    //	numeros = new int [10];
-    	//numeros [0] = 1; /* el elemento 0 es el valor, el 1 es el indice que le doy*/
-    //	numeros [1] = 2;
-    	
-    	/* y asi hasta el indice 9*/
-    	
-    	
-    	
-    	//-------- segunda manera de declarar arrays-------
-    	
-    	
-    		int [] numeros = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    		
-    		String [] nombres = {"Tamara", "Adrian", "Carlos", "Luis", "Ivan", "Maria Jose", "Rosa"};
-    	
-    	/* Para motrar por consola los valores*/
-    		/*Utlizando una funcion pintln mostrar cada elemento del arrays*/
-    		
-    		/*VARIANTE 1*/
-    		
-    		
-    		System.out.println(nombres [0]);
-    		System.out.println(nombres[1]);
-    		
-    		
-    		/* VARIANTE 2*, con sistema de control de flujo que recorra el arrays- 
-    		 * lo vemo en la rama "sentenciasDeControlDeFlujos"*/
-    		
-    	    		
-    		/*Con una sentencia FOR de toda la vida. y tambien con un FOR mejorado - for each- */
-    		
-    		
-    		for(int index =0; index<nombres.length;index++) 
-    			if (nombres [index].length()>4) {
-    				 System.out.println(nombres[index]);
-    			}	
-    		/* que diferencia hay entre utilizar 
-    		 * i++ autoincremento o ++i autodecremento- 
-    		 * si van solo en una sentencia entonces el resultado es el mismo, 
-    		 * pero si van con otras variables */
-    		
-    		/* si quiero mostrar solo los elementos con indice par*/
-    		for(int index=0; index <numeros.length; index++)
-    		{if (index % 2 == 0)
-    			System.out.println("el elemento de indice" +index +", es par");}
     		
     }
 }
